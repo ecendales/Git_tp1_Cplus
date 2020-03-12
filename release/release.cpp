db8674087acfa26543fca9d8d65110fd2f87b0f9
ecendales <ecendales@gmail.com>
2020-03-12 01:55:59 -0400

    code personne 1 mis à jour

--- release/release.cpp
+++ release/release.cpp
@@ -1,10 +1,36 @@
-// release.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
+#include <string>
 
-#include <iostream>
+#include<iostream>
+using namespace std;
+int main() {
+	int num[100];
+	int den[100];
+	int res[100];
+	int n = 3;
+	int i, a, b;
 
-int main()
-{
-    std::cout << "inicio tp1";
+	
+	for (int i = 0; i < n; i++) {
+
+		cout << "Entrez le numrateur de fraction #" << i + 1 << " : " << endl;
+		cin >> num[i];
+		cout << "Entrez le denominateur de fraction #" << i + 1 << " : " << endl;
+		cin >> den[i];
+		a = __max(num[i], den[i]);
+		b = __min(num[i], den[i]);
+
+		do {
+			res[i] = b;
+			b = a % b;
+			a = res[i];
+
+		} while (b != 0);
+	}
+
+
+	
+	return 0;
+
+
+
 }
-
